c02151a1af94c5da934a75e207ba8de2317f2bd3
18292821405 <760218327@qq.com>
2021-01-30 16:06:19 +0800

    Merge branch 'master' of git.dev.tencent.com:feanor/Sreg

--- sreg.py
+++ sreg.py
@@ -212,12 +212,8 @@ def main(Sreg=Sreg):
         passport, passport_type = str(parser_argument.email), "email"
     encoder_obj = OutputEncoder(passport_type, passport)
 
-<<<<<<< HEAD
     for obj in objects:
         obj.set_encoder(encoder_obj)
-=======
-    _ = [obj.set_passport(passport, passport_type) for obj in objects]
->>>>>>> f709cfb35ea026db81740af5ac2beb3b60bed4f5
     # filter plugin type
     # _ = [obj.output() for obj in objects if passport_type in obj.content['type']]
     jobs = [multiprocessing.Process(target=obj.check)
